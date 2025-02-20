4bccdfa28cc2c371b25591d9b45cea1f2b3299bf
Lukas Lannge <lukas.lannge@hotmail.se>
2025-02-20 12:54:31 +0100

    update LTCA

--- KTH BNSS VPKI/create_KTH_LTCA.py
+++ KTH BNSS VPKI/create_KTH_LTCA.py
@@ -4,13 +4,14 @@ import time
 import random
 
 # Dedicated libs
+import requests
 import xmlrpc.client
 from cryptography import x509
-from cryptography.x509.oid import NameOID
+from cryptography.x509.oid import NameOID, ObjectIdentifier
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import ec
-import interfaces_pb2 
+import interfaces_pb2
 
 
 REQ_X509_CERT_REQ_VEHICLE_TO_LTCA_USING_PROTO_BUFF = 122
@@ -29,11 +30,12 @@ def get_x509_csr(hostname: str):
         format=serialization.PrivateFormat.TraditionalOpenSSL,
         encryption_algorithm=serialization.NoEncryption()
     ))
-    
+    email: NameOID = "emailAdress"
     name = x509.Name([
         x509.NameAttribute(NameOID.COUNTRY_NAME, "SE"),
         x509.NameAttribute(NameOID.ORGANIZATION_NAME, "ACME"),
-        x509.NameAttribute(NameOID.COMMON_NAME, hostname)
+        x509.NameAttribute(NameOID.COMMON_NAME, hostname),
+        x509.NameAttribute(email, "lukasla.edu@gmail.com")
     ])
 
     san = x509.SubjectAlternativeName([x509.DNSName(hostname)])
@@ -65,6 +67,8 @@ def request_x509_cert(csr: str):
     msgX509CertReq_V2LTCA.iReqType = 122
     msgX509CertReq_V2LTCA.iLTCAIdRange = 1002
     msgX509CertReq_V2LTCA.strProofOfPossessionVoucher = ""
+    msgX509CertReq_V2LTCA.strKeyUsage = ""
+    msgX509CertReq_V2LTCA.strExtendedKeyUsage = ""
     msgX509CertReq_V2LTCA.strX509CertReq = csr
     msgX509CertReq_V2LTCA.iNonce = random.randint(0, 65535)
     msgX509CertReq_V2LTCA.tTimeStamp = int(time.time())
@@ -75,10 +79,11 @@ def request_x509_cert(csr: str):
     print(serialzed_to_string)
     print("\nEncoded: ")
     print(encoded_req)
-    print("\n")
     # return (0, encoded_req)
-    return clientXML.__request("ltca.operate", (REQ_X509_CERT_REQ_VEHICLE_TO_LTCA_USING_PROTO_BUFF, encoded_req))
-    
+    website = requests.get("https://www.exemple.com")
+    print(website)
+    return clientXML.ltca.operate(REQ_X509_CERT_REQ_VEHICLE_TO_LTCA_USING_PROTO_BUFF, encoded_req)
+
 def main():
     x509_csr: x509.CertificateSigningRequest = get_x509_csr("www.acme.se")
 
@@ -86,11 +91,11 @@ def main():
     x509_csr_string = x509_csr.public_bytes(serialization.Encoding.PEM).decode("UTF-8")
     response = request_x509_cert(x509_csr_string)
 
-    cert: x509.Certificate = cert_from_rpc_response(response)
+    # cert: x509.Certificate = cert_from_rpc_response(response)
 
     # Save Certificate 
-    with open("server_cert.pem", "wb") as f:
-        f.write(cert.public_bytes(serialization.Encoding.PEM))
+    # with open("server_cert.pem", "wb") as f:
+    #     f.write(cert.public_bytes(serialization.Encoding.PEM))
 
 if __name__ == "__main__":
     main()
